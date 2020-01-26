0a0b8e25c86790c95a4d9a6dcff1ed558f919371
Christoph Purrer <christophpurrer@gmail.com>
2020-01-25 18:54:54 -0800

    Add batch file to build with clang using MSVC frontend

---
+++ build-clang-msvc.bat
@@ -0,0 +1,8 @@
+@echo Create object
+clang-cl.exe  /c /I"x64\Debug\Generated Files\\" /Z7 /nologo /W3 /WX- /diagnostics:column /Od /D _DEBUG /D _WINDOWS /D _UNICODE /D UNICODE /EHsc /MDd /GS /fp:precise /permissive- /std:c++latest /Fo"x64\Debug\\" /Gd /TP -m64  /bigobj /await main.cpp
+
+@echo Link objects
+lld-link.exe /OUT:"C:\Users\chpurrer\Documents\GitHub\cppwinrt-clang\x64\Debug\cppwinrt-clang.exe" /INCREMENTAL:NO /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64" /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\ucrt\x64" /LIBPATH:"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\lib\x64" kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib WindowsApp.lib /MANIFEST /ManifestFile:"x64\Debug\cppwinrt-clang.exe.intermediate.manifest" /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /DEBUG:FULL /PDB:"C:\Users\chpurrer\Documents\GitHub\cppwinrt-clang\x64\Debug\cppwinrt-clang.pdb" /SUBSYSTEM:WINDOWS /DYNAMICBASE /NXCOMPAT /IMPLIB:"C:\Users\chpurrer\Documents\GitHub\cppwinrt-clang\x64\Debug\cppwinrt-clang.lib"  x64\Debug\main.obj
+
+@embed manifest
+mt.exe /nologo /verbose /out:"C:\Users\chpurrer\Documents\GitHub\cppwinrt-clang\x64\Debug\cppwinrt-clang.exe.manifest" /manifest cppwinrt.manifest "x64\Debug\cppwinrt-clang.exe.intermediate.manifest
