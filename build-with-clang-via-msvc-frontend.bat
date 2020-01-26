eaecb218fdbf11cbcf7e39a142c03b67420058b1
Christoph Purrer <christophpurrer@gmail.com>
2020-01-25 20:09:03 -0800

    Update batch files to create the projects from the command line

--- build-with-clang-via-msvc-frontend.bat
+++ build-with-clang-via-msvc-frontend.bat
@@ -1,3 +1,5 @@
+set WINDOWS_SDK=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0
+
 @echo Generate C++/WinRT header files
 cppwinrt.exe @cppwinrt_plat.rsp
 
@@ -8,10 +10,10 @@ if not exist build mkdir build
 clang -v
 
 @echo Create object
-clang-cl.exe  /c /I"Generated Files\\" /Z7 /nologo /W3 /WX- /diagnostics:column /Od /D _DEBUG /D _WINDOWS /D _UNICODE /D UNICODE /EHsc /MDd /GS /fp:precise /permissive- /std:c++latest /Fo"build\\" /Gd /TP -m64  /bigobj /await main.cpp
+clang-cl.exe /c /I"Generated Files\\" /D _WINDOWS /D _UNICODE /D UNICODE /EHsc /std:c++latest /Fo"build\\" main.cpp
 
 @echo Link objects
-lld-link.exe /OUT:"build\cppwinrt-clang.exe" /INCREMENTAL:NO /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64" /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\ucrt\x64" /LIBPATH:"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\lib\x64" kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib WindowsApp.lib /MANIFEST /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /SUBSYSTEM:WINDOWS /DYNAMICBASE /NXCOMPAT build\main.obj
+lld-link.exe /OUT:"build\cppwinrt-clang.exe" /INCREMENTAL:NO /LIBPATH:"%WINDOWS_SDK%\um\x64" /LIBPATH:"%WINDOWS_SDK%\ucrt\x64" /LIBPATH:"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\lib\x64" kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib WindowsApp.lib /SUBSYSTEM:WINDOWS build\main.obj
 
 @echo Embed manifest
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\mt.exe" /nologo /verbose /out:"build\cppwinrt-clang.exe.manifest" /manifest cppwinrt.manifest 
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\mt.exe" -manifest cppwinrt.manifest -outputresource:build\cppwinrt-clang.exe;1
--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ if not exist build mkdir build
 clang -v
 
 @echo Create binary
-clang main.cpp -D_CONSOLE -DWIN32_LEAN_AND_MEAN -DWINRT_LEAN_AND_MEAN -DUNICODE -I "Generated Files" -L "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64" -Wl,kernel32.lib,user32.lib,windowsapp.lib -Xclang -std=c++2a -Xclang -Wno-delete-non-virtual-dtor -o build\cppwinrt-clang.exe
+clang -I "Generated Files" -D_WINDOWS -DUNICODE -D_UNICODE -DWIN32_LEAN_AND_MEAN -DWINRT_LEAN_AND_MEAN -DMDd main.cpp -Xclang -std=c++2a -Xclang -Wno-delete-non-virtual-dtor -o build\cppwinrt-clang.exe -L "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64" -Wl,kernel32.lib,user32.lib,windowsapp.lib -DMANIFEST -DMANIFESTUAC:"level='asInvoker' uiAccess='false'"
 
 @echo Embed manifest
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\mt.exe" -manifest cppwinrt-clang.exe.manifest -outputresource:build\cppwinrt-clang.exe;1
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\mt.exe" -manifest cppwinrt.manifest -outputresource:build\cppwinrt-clang.exe;1
